6f30b0457c7a028e0fef90e3a3e285164c5da609
arianto-510 <97211033@student.itk.ac.id>
2023-03-24 08:20:04 +0800

    menambahkan fungsi handler dan routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const Hapi = require("@hapi/hapi");
+const addNoteHandler = require("./handler");
 
 const init = async () => {
   const server = Hapi.server({
     host: "localhost",
     port: 3000,
+    handler: addNoteHandler,
   });
 
   await server.start();
