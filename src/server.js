671f4e5201c680650487f8bf0bb7cbca56fd8cd1
arianto-510 <97211033@student.itk.ac.id>
2023-03-24 08:47:07 +0800

    mencari debug

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const Hapi = require("@hapi/hapi");
-const addNoteHandler = require("./handler");
+const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
     host: "localhost",
     port: 3000,
-    handler: addNoteHandler,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`server berjalan di ${server.info.uri}`);
