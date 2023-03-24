39aeed731c2ebaf1543a82096492298fa8580dac
arianto-510 <97211033@student.itk.ac.id>
2023-03-25 06:13:09 +0800

    up ulang code

---
+++ src/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+(async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  // Add the routes.
+  server.route(routes);
+
+  // Start the server.
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+})();
